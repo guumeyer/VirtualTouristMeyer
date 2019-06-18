46f52c1451b872eeb1f999aaf0b486d971ced5da
Gustavo Meyer <gmeyer@hyperwallet.com>
2019-06-18 09:38:08 -0700

    feat: Initial commit with the project structure

---
+++ VirtualTouristMeyer/ViewControllers/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  VirtualTouristMeyer
+//
+//  Created by Meyer, Gustavo on 6/18/19.
+//  Copyright © 2019 Gustavo Meyer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
